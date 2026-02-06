992b433652c0725d526107f0f076417e1b3641d1
prbion <arbionhaha@gmail.com>
2026-02-06 12:34:13 +0000

    Update ImageSnipper.py

--- ImageSnipper.py
+++ ImageSnipper.py
@@ -4,9 +4,13 @@ import pytesseract
 from PIL import Image
 
 class ImageSnipper:
+    def crop_and_save(self, page, rect, output_path):
 
+        if rect is None:
+            print(f"Empty crop area for {output_path}. Skipping save.")
+            return
 
-    def crop_and_save():
+        pix = page.get_pixmap(clip=rect, matrix=pymupdf.Matrix(2, 2))   # Render the cropped area, matrix is used to increase the resolution of the output image
 
-
-    
+        pix.save(output_path)        # Save the result
+        print(f"Crop saved: {output_path}")
