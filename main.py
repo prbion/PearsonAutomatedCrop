efbc90e4a71ee06cc3b9e2f94b4500beefb0101a
prbion <arbionhaha@gmail.com>
2026-02-06 12:29:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,32 @@ from TaskExtractor import TaskExtractor
 from ImageSnipper import ImageSnipper
 
 
+def main():
+    
+    pdf_manager = PDFManager()
+    extractor = TaskExtractor()
+    snipper = ImageSnipper()
 
-""""
-doc = pymupdf.open("8fm0-01-que-20240514.pdf")
-for page in doc: # iterate the document pages
-  text = page.get_text() # get plain text encoded as UTF-8
+    pdf_path = r"C:\Dev\src\8fm0-21-que-20240518.pdf"  # put the path to your PDF file here
+    pdf_manager.open_file(pdf_path)
 
-  print(text)
 
-""""
+    num_pages = pdf_manager.get_page_count()
+    
+    for i in range(num_pages):
+        page = pdf_manager.get_current_page(i)
+        coords = extractor.find_coordinates(page)
+        
+        # Hol dir jetzt alle einzelnen Bereiche
+        sub_task_crops = extractor.calculate_crop_areas(coords, page)
+        
+        for task_index, crop_rect in enumerate(sub_task_crops):
+            output_name = f"page_{i+1}_task_{task_index+1}.png"
+            snipper.crop_and_save(page, crop_rect, output_name)
+            
+
+
+    pdf_manager.close_pdf()
+
+if __name__ == "__main__":
+    main()
